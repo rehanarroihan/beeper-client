4a58d86be0ce38f1ad6d26f0dad92122ed5fe08b
rehanarroihan <multazamgsd@gmail.com>
2019-03-13 09:45:56 +0700

    define global variable at .env, axios call sample

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -3,18 +3,18 @@
         <h3 class="text-center">Register</h3>
 
         <div class="form-group">
-            <input type="email" class="form-control" placeholder="Email address">
+            <input  type="email" class="form-control" v-model="user.email" placeholder="Email address">
         </div>
         <div class="form-group">
-            <input type="text" class="form-control" placeholder="Username">
+            <input type="text" class="form-control" v-model="user.username" placeholder="Username">
         </div>
         <div class="form-group">
-            <input type="password" class="form-control" placeholder="Password">
+            <input type="password" class="form-control" v-model="user.password" placeholder="Password">
         </div>
 
         <hr>
 
-        <button class="btn btn-lg btn-primary btn-block">Register</button>
+        <button class="btn btn-lg btn-primary btn-block" @click="register()">Register</button>
 
         <p class="text-center m-t-20">
             Already have an account? <router-link to="/auth/login">Login</router-link>
@@ -23,8 +23,29 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'Register'    
+    name: 'Register',
+    data: function() {
+        return {
+            user: {
+                email: "",
+                username: "",
+                password: ""
+            }
+        }
+    },
+    methods: {
+        register() {
+            const {email, username, password} = this.user;
+            axios.post("http://localhost:9090/users", {
+                username, 
+                email, 
+                password
+            }).then(data => console.log(data)).catch(err => console.log(err.code));
+        },
+    },
 }
 </script>
 
